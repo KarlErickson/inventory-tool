9e88eeaf04b2f17e81f17fa4f1e4ba84e72751e4
Karl Erickson <karler@microsoft.com>
2019-02-26 14:52:49 -0800

    generalized go.bat

--- go.bat
+++ go.bat
@@ -1,16 +1,16 @@
-pushd d:\inv\visualstudio-docs-pr
+pushd ..\visualstudio-docs-pr
 git pull origin master
-cd d:\inv\visualstudio-docs-pr
+cd ..\visualstudio-docs-pr
 git pull origin master
-cd d:\inv\azure-docs-pr
+cd ..\azure-docs-pr
 git pull origin master
-cd d:\inv\azure-docs-sdk-java
+cd ..\azure-docs-sdk-java
 git pull origin master
-cd d:\inv\azure-docs-sdk-python
+cd ..\azure-docs-sdk-python
 git pull origin master
-cd d:\inv\azure-docs-sdk-node
+cd ..\azure-docs-sdk-node
 git pull origin master
-cd d:\inv\vsts-docs-pr
+cd ..\vsts-docs-pr
 git pull origin master
 popd
 python take-inventory.py
